4e28daa186f5b8dcf490f59905fd527610ab46bb
Anderson Bravalheri <andersonbravalheri@gmail.com>
2018-06-18 20:28:56 +0000

    Initial commit

---
+++ README.rst
@@ -0,0 +1,19 @@
+===================================
+dummy-pipenv-example-for-pyscaffold
+===================================
+
+
+Add a short description here!
+
+
+Description
+===========
+
+A longer description of your project goes here...
+
+
+Note
+====
+
+This project has been set up using PyScaffold 3.0.3.post0.dev54+ge717b7e.dirty. For details and usage
+information on PyScaffold see http://pyscaffold.org/.
---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+    Setup file for dummy_pipenv_example_for_pyscaffold.
+
+    This file was generated with PyScaffold 3.0.3.post0.dev54+ge717b7e.dirty.
+    PyScaffold helps you to put up the scaffold of your new Python project.
+    Learn more under: http://pyscaffold.org/
+"""
+from setuptools import setup
+
+
+if __name__ == "__main__":
+    setup(use_pyscaffold=True)
